9b086aad66a7f99e012cff20f0da87a79367c17f
dhoat30 <dhoat30@gmail.com>
2026-05-10 09:35:46 +1200

    removed gtm

--- app/layout.js
+++ app/layout.js
@@ -30,35 +30,13 @@ const workSans = Work_Sans({
 
 export default function RootLayout({ children }) {
 
-  const GTM_ID = process.env.GTM_ID;
 
   return (
     <html lang="en" >
- <Script
-          id="gtm-script"
-          strategy="lazyOnload" // or "lazyOnload" if you prefer
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s);j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `
-          }}
-        />
+
               <body className={`${prompt.variable} ${workSans.variable}`}>
                   {/* 3) GTM noscript fallback */}
-          <noscript>
-          <iframe 
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} 
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-            loading='lazy'
-          />
-        </noscript>
+ 
         {/* <AppRouterCacheProvider> */}
       <ClientProvider>
           {children}
